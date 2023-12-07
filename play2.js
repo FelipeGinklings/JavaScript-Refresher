36fc60cc386c9d80ac9255d669f871901005da74
FelipeGinklings <felipe.ginklings@gmail.com>
2023-12-07 09:57:14 -0300

    Destructuring

--- play2.js
+++ play2.js
@@ -9,10 +9,21 @@ const person = {
     // }
 };
 
-const copiedPerson = { ...person };
-console.log(copiedPerson);
+const printName = ({ name }) => {
+    console.log(name);
+};
+
+printName(person);
+
+const { name, age } = person;
+console.log(name, age);
+
+// const copiedPerson = { ...person };
+// console.log(copiedPerson);
 
 const hobbies = ['Sports', 'Cooking'];
+const [hobby1, hobby2] = hobbies;
+console.log(hobby1, hobby2);
 // for (let hobby of hobbies) {
 //   console.log(hobby);
 // }
@@ -24,8 +35,8 @@ const hobbies = ['Sports', 'Cooking'];
 // console.log(hobbies);
 
 // const copiedArray = hobbies.slice();
-const copiedArray = [...hobbies];
-console.log(copiedArray);
+// const copiedArray = [...hobbies];
+// console.log(copiedArray);
 
-const toArray = (...args) => args;
-console.log(toArray(1, 2, 3, 4));
+// const toArray = (...args) => args;
+// console.log(toArray(1, 2, 3, 4));
