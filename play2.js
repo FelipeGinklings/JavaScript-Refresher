0cca1603135fd6a78a376788f8cb12db4046099c
FelipeGinklings <felipe.ginklings@gmail.com>
2023-12-07 09:48:34 -0300

    Spread Operator & Rest Parameters

--- play2.js
+++ play2.js
@@ -9,11 +9,23 @@ const person = {
     // }
 };
 
-const hobbies = ['Sports', 'Cooking'];
+const copiedPerson = { ...person };
+console.log(copiedPerson);
 
+const hobbies = ['Sports', 'Cooking'];
 // for (let hobby of hobbies) {
 //   console.log(hobby);
 // }
 
-console.log(hobbies.map((hobby) => 'Hobby: ' + hobby));
-console.log(hobbies);
+// console.log(hobbies.map((hobby) => 'Hobby: ' + hobby));
+// console.log(hobbies);
+
+// hobbies.push('Programming')
+// console.log(hobbies);
+
+// const copiedArray = hobbies.slice();
+const copiedArray = [...hobbies];
+console.log(copiedArray);
+
+const toArray = (...args) => args;
+console.log(toArray(1, 2, 3, 4));
