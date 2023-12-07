1e920daf5af2849e8eeed1f23f6e274005cf5671
FelipeGinklings <felipe.ginklings@gmail.com>
2023-12-07 09:22:07 -0300

    Arrays & arrays methods, map function

--- play2.js
+++ play2.js
@@ -2,11 +2,18 @@ const person = {
     name: 'Felipe',
     age: 29,
     greet() {
-      console.log('Hello, I am ' + this.name);
-    }
+        console.log('Hello, I am ' + this.name);
+    },
     // greet: function() {
     //   console.log('Hello, I am ' + this.name);
     // }
 };
 
-person.greet();
+const hobbies = ['Sports', 'Cooking'];
+
+// for (let hobby of hobbies) {
+//   console.log(hobby);
+// }
+
+console.log(hobbies.map((hobby) => 'Hobby: ' + hobby));
+console.log(hobbies);
